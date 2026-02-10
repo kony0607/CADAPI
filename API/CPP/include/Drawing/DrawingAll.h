3888ea7b34ae7ddc4b565670a98c90eaf972b1f7
kony0607 <miyu2004060@gmail.com>
2026-02-10 11:17:26 +0900

    Keep scripts only

--- API/CPP/include/Drawing/DrawingAll.h
+++
@@ -1,6 +0,0 @@
-#include <Drawing/DrawingTypeDefs.h>
-#include <Drawing/Drawing/Drawing.h>
-#include <Drawing/Drawing/DrawingDocument.h>
-#include <Drawing/Drawing/DrawingExportManager.h>
-#include <Drawing/Drawing/DrawingExportOptions.h>
-#include <Drawing/Drawing/PDFExportOptions.h>
